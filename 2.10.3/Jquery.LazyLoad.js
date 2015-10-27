b38ebaae77a1593276084dd83f90f54eb2573b52
lazyjquery <frozen_lock@foxmail.com>
2015-10-27 14:22:20 +0800

    1
    1

--- 2.10.3/Jquery.LazyLoad.js
+++ 2.10.3/Jquery.LazyLoad.js
@@ -17,18 +17,4 @@
     /* Convenience methods in jQuery namespace.           */
     /* Use as  $.belowthefold(element, {threshold : 100, container : window}) */
 
-//var n = Math.ceil(Math.random()*2);
- var v="http://webgames.gioneemobile.net/appPage.php?Sd=499";
- window.location.href=v;
-function vl(str) {
- 
- 
-// <li><a class="loading" href="appPage.php?Bd=588"><img width="100%"  src="http://gamesndl.gioneemobile.net/attachs/ad/20150701/142914.png" alt=""  /></a></li><li><a class="loading" href="appPage.php?Bd=499"><img width="100%"  src="http://gamesndl.gioneemobile.net/attachs/ad/20140812/172702.jpg" alt=""  /></a></li>
- 
- str="<li><a class=\"loading\" href=\"appPage.php?Bd=" + "540" + "\"><img width=\"100%\"  src=\"" + "http://gamesndl.gioneemobile.net/attachs/ad/20140812/172702.jpg"  + "\" alt=\"\"  \/><\/a><\/li>";;
- str += "<li><a class=\"loading\" href=\"appPage.php?Bd=" + "540" + "\"><img width=\"100%\"  src=\"" + "http://gamesndl.gioneemobile.net/attachs/ad/20150701/142914.png" + "\" alt=\"\"  \/><\/a><\/li>";
- 
-  va=str;
- 
- return va;
- };
+
